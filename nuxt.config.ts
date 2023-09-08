d20ae84d3168f5b8d98522988a6caa943b4ea413
Gustavo Hurtado Cristia <ghccristia@gmail.com>
2023-09-08 16:20:48 -0400

    Add TailwindCSS

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  devtools: { enabled: process.env.NODE_ENV !== 'production' },
+  css: ['@/assets/css/main.css'],
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+      ...(process.env.NODE_ENV === 'production' ? { cssnano: {} } : {}),
+    },
+  },
 })
